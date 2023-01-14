4f4fa5757a8a1b5752cf664dd229742771bc13e3
ytcalifax <trayanovboris@gmail.com>
2023-01-14 14:52:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='PyHasteBin',
-    version='1.2.2',
+    version='1.2.3',
     packages=['postbin', "postbin.v2"],
     url='https://github.com/ytcalifax/PyHasteBin',
     python_requires=">=3.6",
